c4f84ce56fae7205e9162a55b571c3ed4de65b1d
SouzaRafaela <souzabarbosa.rafaela@gmail.com>
2021-08-15 16:09:32 -0300

    primeiro commit

---
+++ app/src/main/java/co/rafaelasouza/beberagua/MainActivity.java
@@ -0,0 +1,21 @@
+package co.rafaelasouza.beberagua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+public class MainActivity<button> extends AppCompatActivity {
+
+    private button btnNotify;
+    private EditText editMinute;
+    private TimePicker timePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
